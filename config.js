273d2d44dbcbcc88e9c68a36a03e82fee01b9d6d
web-static-host <57108841+web-static-host@users.noreply.github.com>
2026-02-04 15:16:58 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Файл только для ссылок и настроек
+const CONFIG = {
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbxxkNEExbKAiOjTcbsL4XsNFAeRT7MXhQNTvk_5cD3fFJVHGTwcZtlRRuM0QMDXxvK7/exec',
+    MANAGER: {
+        NAME: "Иванов Иван",
+        DEPT: "Отдел продаж"
+    }
+};
